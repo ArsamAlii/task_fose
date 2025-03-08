80d481dc749a38f65921f8c872998e86abaaddc5
Arsam Ali <p230038@pwr.nu.edu.pk>
2025-03-08 18:12:28 +0500

    conflict resolved arsamali 6:08 pm 8 march 2025

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print("This is Versioning Practically")
+print("fine, but aik post tou bnti ha na :(")
+print("Now Git will become my skill set but i won't do chichorapan on LinkedIn but add it as skill")
